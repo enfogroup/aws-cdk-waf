5ddeeff1e8a59f96a3ba2d49976f288b55f06dc6
Urban Pettersson <urbanpe88@gmail.com>
2022-06-07 07:38:16 +0200

    chore: run cdk init

---
+++ lib/index.ts
@@ -0,0 +1,20 @@
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export interface AwsCdkWafProps {
+  // Define construct properties here
+}
+
+export class AwsCdkWaf extends Construct {
+
+  constructor(scope: Construct, id: string, props: AwsCdkWafProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsCdkWafQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
